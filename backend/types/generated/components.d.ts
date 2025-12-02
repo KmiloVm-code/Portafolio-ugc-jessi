715d8b49c3b65fe621f1dfbcbfe91a6eb9d776cb
KmiloVm <kmilo7v@gmail.com>
2025-12-01 23:02:48 -0500

    feat: Add home page API with hero section and social links components.

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,3 +1,40 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface ComponentSocialLinks extends Struct.ComponentSchema {
+  collectionName: 'components_component_social_links';
+  info: {
+    displayName: 'Social Links';
+    icon: 'twitter';
+  };
+  attributes: {
+    href: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'#'>;
+    icon: Schema.Attribute.Media<'images'>;
+    isExternal: Schema.Attribute.Boolean;
+    label: Schema.Attribute.String;
+  };
+}
+
+export interface LayoutHeroSection extends Struct.ComponentSchema {
+  collectionName: 'components_layout_hero_sections';
+  info: {
+    displayName: 'Hero Section';
+    icon: 'stack';
+  };
+  attributes: {
+    description: Schema.Attribute.Text;
+    portada: Schema.Attribute.Media<'images'>;
+    social: Schema.Attribute.Component<'component.social-links', true>;
+    title: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'component.social-links': ComponentSocialLinks;
+      'layout.hero-section': LayoutHeroSection;
+    }
+  }
+}
